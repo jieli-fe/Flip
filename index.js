0318e1d6211fd51cbb58ce49779591137c72e44d
lijie <14734576@qq.com>
2024-08-18 12:23:47 +0800

    init

---
+++ index.js
@@ -0,0 +1,59 @@
+export default function FlipAnimate(
+    elementList,
+    options = {
+        duration: 600,
+        easing: "ease",
+    }
+) {
+    this.elements = Array.from(elementList);
+    this.options = options;
+    this.firstPosition = [];
+    this.lastPosition = [];
+    this.first();
+    this.destroy();
+    console.log("this.elements", this.elements);
+}
+FlipAnimate.prototype.play = function () {
+    this.last();
+    this.invert();
+};
+FlipAnimate.prototype.first = function () {
+    this.firstPosition = this.getPosition();
+    console.log("first position", this.firstPosition);
+};
+
+FlipAnimate.prototype.last = function () {
+    this.lastPosition = this.getPosition();
+    console.log("last position", this.lastPosition);
+};
+
+FlipAnimate.prototype.getPosition = function () {
+    return this.elements.map((el, index) => el.getBoundingClientRect());
+};
+FlipAnimate.prototype.invert = function () {
+    const diff = this.firstPosition.map((first, index) => {
+        const last = this.lastPosition[index];
+        return {
+            left: first.left - last.left,
+            top: first.top - last.top,
+            width: first.width - last.width,
+            height: first.height - last.height,
+        };
+    });
+    console.log("diff", diff);
+
+    this.elements.forEach((ele, index) => {
+        const { left, top, width, height } = diff[index];
+        ele.animate(
+            [
+                {
+                    transform: `translate3d(${left}px, ${top}px, 0) `,
+                },
+                { transform: `translate3d(0, 0, 0) scale(1, 1)` },
+            ],
+            {
+                ...this.options,
+            }
+        );
+    });
+};
---
+++ readme.md
@@ -0,0 +1,18 @@
+## 1. 导入 index.js 文件
+
+```js
+import FlipAnimate from "./index.js";
+```
+
+## 2. 初始化卡片
+
+```js
+//把需要添加动画的卡片放插入到容器中
+const animate = new FlipAnimate(document.querySelectorAll(".cardBox"));
+```
+
+## 3. 当卡片添加或者删除后执行
+
+```js
+animate.play();
+```
